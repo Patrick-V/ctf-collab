c381e7f483d4f7555d8640d025d1c40dc3fd5441
Patrick-V <13681049+Patrick-V@users.noreply.github.com>
2021-03-09 15:03:37 +0100

    Finished stage 0

--- env.py
+++ env.py
@@ -2,5 +2,5 @@ config = {}
 
 config['WEBEX_BASE_URL'] = "https://webexapis.com"
 config['WEBEX_ACCESS_TOKEN'] = "MDBlMzFiZWYtNzY2NS00YmRmLWE2ZjAtNTIwOTUxMmJiMjU5OGI2MTkxNmQtNjNk_PF84_1eb65fdf-9643-417f-9974-ad72cae0e10f"
-config['PRODUCTION_ROOM'] = "<Replace me in stage 0>"
+config['PRODUCTION_ROOM'] = "Y2lzY29zcGFyazovL3VzL1JPT00vM2ZmOGU3ZjAtODAwOS0xMWViLTg4NjUtNTE2OGI4NmU0NjA4"
 config['TESTING_ROOM'] = "<Replace me in stage 1>"
---
+++ stage0.py
@@ -0,0 +1,35 @@
+from env import config
+import requests
+
+space_name = 'CSAP Programmability CTF - Team 2'
+
+headers = {
+    'Authorization': f"Bearer {config['WEBEX_ACCESS_TOKEN']}",
+    'Content-Type': 'application/json',
+}
+
+def get_list_rooms():
+    response = requests.get(f"{config['WEBEX_BASE_URL']}/v1/rooms", headers = headers)
+
+    if response.status_code == 200:
+        list_rooms = response.json()['items']
+        return list_rooms
+    else:
+        return None
+
+def get_room_id(space_name):
+    list_rooms = get_list_rooms()
+
+    if list_rooms == None:
+        return None
+    else:
+        for room in list_rooms:
+            if room['title'] == space_name:
+                space_id = room['id']
+        
+        return space_id
+
+if __name__ == '__main__':
+    space_id = get_room_id(space_name)
+
+    print(f"{space_id} is the ID belonging to space {space_name}")
